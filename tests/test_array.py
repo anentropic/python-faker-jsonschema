d11a358f06d55444a8f39d65cb500c770d61cce0
Anentropic <ego@anentropic.com>
2020-04-24 20:21:47 +0100

    some more fixes, basic array gen test working

--- tests/test_array.py
+++ tests/test_array.py
@@ -2,15 +2,38 @@ import pytest
 from jsonschema import validate
 
 
-# @pytest.mark.flaky(max_runs=50, min_passes=50)
 @pytest.mark.parametrize(
     "schema",
     (
         {},
+        {"type": "string"},
+        # example from "petstore-expanded.yml"...
+        {
+            "allOf": [
+                {
+                    "type": "object",
+                    "required": ["id"],
+                    "properties": {
+                        "id": {"type": "integer", "format": "int64"},
+                    },
+                },
+                {
+                    "type": "object",
+                    "required": ["name"],
+                    "properties": {
+                        "name": {"type": "string"},
+                        "tag": {"type": "string"},
+                    },
+                },
+            ]
+        },
     )
 )
 def test_jsonschema_array_from_schema(
-    faker, schema
+    faker, repeats, schema
 ):
-    result = faker.jsonschema_array()
-    assert isinstance(result, list)
+    for _ in range(repeats):
+        result = faker.jsonschema_array(items=schema)
+        assert isinstance(result, list)
+        for item in result:
+            validate(item, schema)
